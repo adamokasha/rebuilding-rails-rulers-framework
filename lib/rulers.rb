2b61a2a504dd9d58010bcf2a1fb809cf542dcb33
Adam Okasha <adam.okasha@shopify.com>
2023-02-07 11:55:53 -0500

    Chapter 2 - Controllers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,38 @@
 require "rulers/version"
 require "array"
+require "routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {"Content-Type" => "text/html"}, []]
+      end
+
+      if env["PATH_INFO"] == "/"
+        return [302, {"Location" => "/quotes/a_quote"}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+
+      begin
+        text = controller.send(act)
+      rescue
+        return [500, {'Content-Type' => 'text/html'}, ["Internal server error."]]
+      end
+
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
